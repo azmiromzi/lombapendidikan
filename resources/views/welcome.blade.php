b80f441e74f2587b7f7f3f6f8c1a2d8ddadd3f3f
BoengGit <edukasiasqor@gmail.com>
2022-08-22 14:55:40 +0700

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
   <body>
     <nav class="navbar navbar-expand-lg bg-white fixed-top">
         <div class="container ">
-            <a class="navbar-brand d-flex m-0  p-0" href="#" style="font-family: 'Open Sans', sans-serif;">IT Material </a>
+            <a class="navbar-brand d-flex m-0  p-0" href="#" style="font-family: 'Open Sans', sans-serif; font-size: 2rem">IT Material </a>
               {{-- <img src="{{ asset('image/logo.png') }}" alt="" width="50" height="44" class="d-inline-block align-text-top "> --}}
               {{-- <p class="text-wrap  text-center " >IT Material</p> --}}
         
@@ -35,142 +35,42 @@
           <div class="collapse navbar-collapse " id="navbarNav">
             <ul class="navbar-nav ms-auto">
             
-
+              
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Category Material
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Learning Path
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Laravel</a></li>
+                  <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li><hr class="dropdown-divider"></li>
+
                   <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
               </li>
               <li class="nav-item pe-xl-4 border-login">
                 <a class="nav-link" href="#">About Us</a>
               </li>
-              <li class="nav-item ps-xl-4">
+             
+
+            </ul>
+            <ul class="navbar-nav ms-auto">
+               <li class="nav-item ps-xl-4">
                 <a class="nav-link" href="{{ route('register') }}">Register</a>
               </li>
               <li class="nav-item ps-xl-4">
                 <a class="nav-link" href="{{ route('login') }}">Login</a>
               </li>
-
             </ul>
           </div>
         </div>
     </nav>
 
-      <div class="container mt-5 pt-5">
-        <img src="{{ asset('image/image-welcome1.png') }}" class="img-fluid" alt="...">
-      </div>
+     
+       
 
-    <div class="container mt-4">
-        <h3 class="text-capitalize fw-bolder color-subjudul">
-            some basic material
-        </h3>
-
-        <div class="row">
-            <div class="col-xl-3 col-md-6">
-                <div class="card width-card mt-3 mt-xl-0 box-shadow" >
-                    <img src="{{ asset('image/html.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="oval"></div>
-                        <p class="card-stemple fw-bolder text-capitalize">recomended</p>
-                      <a href="#" class="card-title color-subjudul fw-bolder fs-4 text-decoration-none  d-block distance"  >Basic HTML</a>
-                      <p class="card-text distance ms-2 text-secondary">here is a place to find material about IT</p>
-                      <div class="d-flex justify-content-end distance">
-                          <p class="fw-bolder">209 preview</p>
-                          <a href="#" class="text-decoration-none ms-2">
-                            <i class="bi bi-chat-left-dots-fill fs-5 text-dark"></i>
-                          </a>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card width-card mt-3 mt-xl-0 box-shadow" >
-                    <img src="{{ asset('image/html.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="oval"></div>
-                        <p class="card-stemple fw-bolder text-capitalize">recomended</p>
-                      <a href="#" class="card-title color-subjudul fw-bolder fs-4 text-decoration-none  d-block distance"  >Basic HTML</a>
-                      <p class="card-text distance ms-2 text-secondary">here is a place to find material about IT</p>
-                      <div class="d-flex justify-content-end distance">
-                          <p class="fw-bolder">209 preview</p>
-                          <a href="#" class="text-decoration-none ms-2">
-                            <i class="bi bi-chat-left-dots-fill fs-5 text-dark"></i>
-                          </a>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card width-card mt-3 mt-xl-0 box-shadow" >
-                    <img src="{{ asset('image/html.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="oval"></div>
-                        <p class="card-stemple fw-bolder text-capitalize">recomended</p>
-                      <a href="#" class="card-title color-subjudul fw-bolder fs-4 text-decoration-none  d-block distance"  >Basic HTML</a>
-                      <p class="card-text distance ms-2 text-secondary">here is a place to find material about IT</p>
-                      <div class="d-flex justify-content-end distance">
-                          <p class="fw-bolder">209 preview</p>
-                          <a href="#" class="text-decoration-none ms-2">
-                            <i class="bi bi-chat-left-dots-fill fs-5 text-dark"></i>
-                          </a>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card width-card mt-3 mt-xl-0 box-shadow" >
-                    <img src="{{ asset('image/html.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="oval"></div>
-                        <p class="card-stemple fw-bolder text-capitalize">recomended</p>
-                      <a href="#" class="card-title color-subjudul fw-bolder fs-4 text-decoration-none  d-block distance"  >Basic HTML</a>
-                      <p class="card-text distance ms-2 text-secondary">here is a place to find material about IT</p>
-                      <div class="d-flex justify-content-end distance">
-                          <p class="fw-bolder">209 preview</p>
-                          <a href="#" class="text-decoration-none ms-2">
-                            <i class="bi bi-chat-left-dots-fill fs-5 text-dark"></i>
-                          </a>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-
-        </div>
-        <div class="d-xl-flex justify-content-end d-none">
-            <a href="#">See more...</a>
-        </div>
-    </div>
-
-    <div class="container-fluid mt-4 mb-5 ">
-        <div class="container">
-            <h3 class="text-capitalize fw-bolder color-subjudul">
-                general information
-            </h3>
-        </div>
-    </div>
-
-    <div class="card bg-dark text-white p-0  mb-5 bg-dark">
-        <img src="{{ asset('image/generalinformation.png') }}" class="card-img w-100 border m-0" alt="...">
-        <div class="card-img-overlay">
-
-            <div class="row align-items-center mt-5">
-                <div class="col-md-6 ">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur esse facere accusantium dolor nisi impedit dignissimos minus, dicta nesciunt eveniet voluptatum nostrum odio unde optio fugit saepe? Modi, cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, consequuntur.</p>
-                </div>
-                <div class="col-md-4 offset-2">
-                   <button class="btn btn-lg btn-primary d-inline-block">Lihat Lainnya</button>
-                </div>
-            </div>
-
-        </div>
-    </div>
+      
 
     <!-- Footer -->
 <footer class="text-center text-lg-start bg-white text-muted">
@@ -298,6 +198,7 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    <script src=""></script>
 
   </body>
 </html>
