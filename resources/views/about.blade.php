0336b61ffc598d2073825434c181db43abff11fe
azmi romzi <azmiromzihoesaini@gmail.com>
2022-08-28 20:34:18 +0700

    visi-misi

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -53,7 +53,42 @@
                             here
                         </p>
                     </div>
-                    <button type="button" class="btn button primary-button text-uppercase">Download cv</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="visimisi-area">
+        <div class="container-fluid  p-5">
+            <div class="row justify-content-evenly">
+                <div class="col-lg-4 text-center align-items-center">
+                    <h3 class="mt-lg-5 mb-lg-5">Vision</h3>
+                    <h5 class="mb-lg-3" style="letter-spacing: 1px">Shaping the future and humanity through technology</h5>
+                    <p class="">We understand Technology is an important part of our society and will deliver us to a brighter future. But in the meantime, we also realized human is the most crucial part to encourage technology and make a brighter future for the world.</p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <div class="about-image ">
+
+                        <img src="{{ asset('img/marketing/Elements/Elements02.svg') }}" style="width: 350px" class="img-fluid " alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <h3 class="mt-lg-5 mb-lg-5">Mision</h3>
+                    <div class="row jistify-content-between">
+                        <div class="col-lg-4">
+                            <i class="bi bi-code fs-1"></i>
+                            <p>Web Development</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="bi bi-code fs-1"></i>
+                            <p>Web Development</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="bi bi-code fs-1"></i>
+                            <p>Web Development</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
